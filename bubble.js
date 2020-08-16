3efb98a02c38d32186eb023826e0ddc140fd6856
Seen3 <63313585+Seen3@users.noreply.github.com>
2020-08-16 20:08:56 +0530

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -1,1 +1,25 @@
 //This is a program which implements Bubble Sort in javaScript
+
+function bub(arr)
+{
+  let len=arr.length;
+  for (let i=0;i<len-1;i++)
+  {
+    let swapped=false;
+    for(let j=0;j<len-1;j++)
+    {
+      if(arr[j]>arr[j+1])
+      {
+        let temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+        swapped=true;
+      }
+    }
+    if(!swapped)
+    {
+      break;
+    }
+  }
+  return arr;
+}
